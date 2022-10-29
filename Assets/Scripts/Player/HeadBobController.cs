eaa3014dd8e3e87e0d6315775dfac3b1f6d4da6f
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-10-29 05:34:43 +0200

    heh

---
+++ Assets/Scripts/Player/HeadBobController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadBobController : MonoBehaviour
+{
+    [SerializeField, Range(0, 0.1f)] 
+    private float amplitude;
+    [SerializeField, Range(0, 30)] 
+    private float frequency;
+    [Space] 
+    [SerializeField] 
+    private Transform camera;
+    [SerializeField] 
+    private Transform cameraHolder;
+
+    private float toggleSpeed = 3.0f;
+    private Vector3 startPos;
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        startPos = camera.localPosition;
+    }
+    
+    void Update()
+    {
+        CheckMotion();
+        camera.LookAt(FocusTarget());
+    }
+
+    private void CheckMotion()
+    {
+        float speed = new Vector3(controller.velocity.x, 0, controller.velocity.z).magnitude;
+        ResetPosition();
+        if(speed < toggleSpeed) return;
+        if(!controller.isGrounded) return;
+
+        PlayMotion(FootStepMotion());
+    }
+
+    private Vector3 FootStepMotion()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.deltaTime * frequency) * amplitude;
+        pos.x += Mathf.Cos(Time.deltaTime * frequency / 2) * amplitude * 2;
+        return pos;
+    }
+    
+    private void PlayMotion(Vector3 motion){
+        camera.localPosition += motion; 
+    }
+
+    private void ResetPosition()
+    {
+        if(camera.localPosition == startPos) return;
+        camera.localPosition = Vector3.Lerp(camera.localPosition, startPos, 1 * Time.deltaTime);
+    }
+
+    private Vector3 FocusTarget()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + cameraHolder.localPosition.y,
+            transform.position.z);
+        pos += cameraHolder.forward * 15f;
+        return pos;
+    }
+}
---
+++ Assets/Scripts/Player/WeaponSway.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponSway : MonoBehaviour
+{
+    [SerializeField] public InputActionReference lookInput;
+    
+    [Space] [Header("Sway Settings")]
+    [SerializeField] 
+    private float speed;
+    [SerializeField] 
+    private float sensitivityMultiplier;
+
+    private float yaw;
+    private float pitch;
+    private float yawChange;
+    private float pitchChange;
+
+    private void Update()
+    {
+        Quaternion rotationX = Quaternion.AngleAxis(-yawChange, Vector3.right);
+        Quaternion rotationY = Quaternion.AngleAxis(pitchChange, Vector3.up);
+
+        Quaternion targetRotation = rotationX * rotationY;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, speed * Time.deltaTime);
+    }
+    
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 look = context.ReadValue<Vector2>();
+        look *= sensitivityMultiplier;
+        yawChange = look.x;
+        pitchChange = look.y;
+    }
+
+    private void OnEnable()
+    {
+        lookInput.action.Enable();
+        lookInput.action.performed += OnLook;
+    }
+    
+    private void OnDisable()
+    {
+        lookInput.action.Disable();
+        lookInput.action.performed -= OnLook;
+    }
+}
